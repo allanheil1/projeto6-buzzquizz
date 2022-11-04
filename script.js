2162b170c0dff24437f1ea95132bd8f1f5bfec52
Victoria <vic_andrade_@hotmail.com>
2022-11-04 14:13:53 -0300

    Comportamento de respostas

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // PAGE 3
 
 let quizzTitle, quizzURLImage, quizzQuestionCount, quizzLevelCount;
@@ -10,41 +9,41 @@ Seu título ficou entre 20 caracteres e 65 caracteres;
 Se foi inserida uma URL válida na sua imagem; 
 Se selecionou pelo menos 3 perguntas;
 Se selecionou no mínimo 2 níveis.
-`
+`;
 
-let mainContent = document.getElementById('main-content');
+let mainContent = document.getElementById("main-content");
 
-function verifyValuesQuizzFirstPage () {
-    quizzTitle = document.getElementById('quizz-title').value;
-    quizzURLImage = document.getElementById('quizz-URL-Image').value;
-    quizzQuestionCount = document.getElementById('quizz-question-count').value;
-    quizzLevelCount = document.getElementById('quizz-level-count').value;
+function verifyValuesQuizzFirstPage() {
+  quizzTitle = document.getElementById("quizz-title").value;
+  quizzURLImage = document.getElementById("quizz-URL-Image").value;
+  quizzQuestionCount = document.getElementById("quizz-question-count").value;
+  quizzLevelCount = document.getElementById("quizz-level-count").value;
 
-    if (
-        quizzTitle.length < 20 ||
-        quizzTitle.length > 65 || 
-        !quizzURLImage.startsWith('https://') || 
-        quizzQuestionCount < 3 ||
-        quizzLevelCount < 2
-        ) {
-            alert(ErrorMessage)
-        } else {
-            handleGoToQuizzPage2();
-        }
+  if (
+    quizzTitle.length < 20 ||
+    quizzTitle.length > 65 ||
+    !quizzURLImage.startsWith("https://") ||
+    quizzQuestionCount < 3 ||
+    quizzLevelCount < 2
+  ) {
+    alert(ErrorMessage);
+  } else {
+    handleGoToQuizzPage2();
+  }
 }
 
 function handleGoToQuizzPage2() {
-    let baseToPrint = `
+  let baseToPrint = `
     <div class="form-container" id="second-form">
         <h1 class="form-title">Crie suas perguntas</h1>
 
         <form action="#">
         </form>
     </div>
-    `
+    `;
 
-    mainContent.innerHTML = baseToPrint;
-    let toPrint = `
+  mainContent.innerHTML = baseToPrint;
+  let toPrint = `
     <div class="form-container" id="second-form">
         <h1 class="form-title">Crie suas perguntas</h1>
 
@@ -146,16 +145,14 @@ function handleGoToQuizzPage2() {
 			Prosseguir pra criar níveis
 		  </button>     
       </div>
-    `
-    mainContent.innerHTML = toPrint;
+    `;
+  mainContent.innerHTML = toPrint;
 }
 
-function verifyValuesQuizzSecondPage () {
+function verifyValuesQuizzSecondPage() {}
 
-}
-
-function handleGoToQuizzPage3 () {
-    let toPrint = `
+function handleGoToQuizzPage3() {
+  let toPrint = `
     <div class="form-container" id="third-form">
         <h1 class="form-title">Agora, decida os níveis</h1>
 
@@ -208,13 +205,13 @@ function handleGoToQuizzPage3 () {
           Finalizar Quizz
         </button>
       </div>
-    `
+    `;
 
-    mainContent.innerHTML = toPrint;
+  mainContent.innerHTML = toPrint;
 }
 
-function handleGoToQuizzPage4 () {
-    let toPrint = `
+function handleGoToQuizzPage4() {
+  let toPrint = `
     <div class="form-container" id="first-form">
     <h1 class="form-title">Seu quizz está pronto!</h1>
 
@@ -234,9 +231,174 @@ function handleGoToQuizzPage4 () {
         Voltar pra home
     </button>
   </div>
-    `
+    `;
 
-    mainContent.innerHTML = toPrint;
+  mainContent.innerHTML = toPrint;
 }
 
 // PAGE 3 END
+
+// PAGE 2
+let qtdPerguntas;
+
+function responder() {
+  const quizz = document.querySelector("main");
+  // ALTERAR ID BASEADO NA TELA 1 !
+  axios
+    .get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/16233")
+    .then((response) => {
+      let item = response.data;
+
+      levelsArray = item.levels;
+
+      let add = `<div class="titulo">
+              <div class="black"><img src="${item.image}" alt="Titulo"></div>
+              <h1>${item.title}</h1>
+          </div>`;
+
+      qtdPerguntas = item.questions.length;
+      console.log(qtdPerguntas)
+
+      for (var i = 0; i < item.questions.length; i++) {
+        const quantidadeDeRespostas = item.questions[i].answers;
+        const sortido = quantidadeDeRespostas.sort(baguncar);
+
+        function baguncar() {
+          return 0.5 - Math.random();
+        }
+        add += `<div class="quizz">
+                  <div class="pergunta">
+                    <div class="cabecalho" style="background-color:${item.questions[i].color}">
+                      <p class="ctitulo">${item.questions[i].title}</p>
+                    </div>`;
+
+        let contador = 0;
+        let quantidadeDePerguntas = sortido.length;
+        for (var j = 0; j < quantidadeDePerguntas; j++) {
+          contador++;
+          if (contador == 1) {
+            add += `<div class="quadro">
+                        <div class="resposta ${sortido[j].isCorrectAnswer} quiz${i}${j}">
+                          <img src="${sortido[j].image}" alt="quiz${i}${j}">
+                          <p class="paragrafo">${sortido[j].text}</p>
+                        </div>`;
+            if (quantidadeDePerguntas == 3) {
+              add += `</div>
+                      </div>`;
+            }
+          }
+          if (contador == 2) {
+            add += `<div class="resposta ${sortido[j].isCorrectAnswer} quiz${i}${j}">
+                          <img src="${sortido[j].image}" alt="quiz${i}${j}">
+                          <p class="paragrafo">${sortido[j].text}</p>
+                        </div>
+                    </div>`;
+
+            if (quantidadeDePerguntas == 2 || j == 3) {
+              add += `</div>
+                      </div>`;
+            }
+
+            contador = 0;
+          }
+        }
+      }
+
+      quizz.innerHTML = add;
+    })
+
+    .catch((error) => {
+    });
+}
+const main = document.querySelector('main')
+let contador = 0;
+let contador2 = 1;
+main.addEventListener("click", function (e) {
+    let alt = e.srcElement.alt
+    console.log(alt)
+    if (!alt.includes('quiz') || alt.length == 0) {
+        return;
+    }
+
+    let resposta_click = document.querySelector(`.${alt}`)
+    resposta_click.classList.add("escolhido");
+
+    let resposta = document.querySelectorAll('.resposta')
+
+    for(let i = 0; i < resposta.length; i++){
+        if(!resposta[i].className.includes('escolhido'))
+            {
+                if(alt[4] == resposta[i].className.split('quiz')[1][0])
+                {
+                    resposta[i].classList.add("desabilitado");
+                }
+                
+            }
+        
+        if(resposta[i].className.includes('false'))
+        {
+            if(alt[4] == resposta[i].className.split('quiz')[1][0])
+            {
+                resposta[i].classList.add("errado");
+            }
+            
+        }
+
+        else if(resposta[i].className.includes('true'))
+        {
+            if(alt[4] == resposta[i].className.split('quiz')[1][0])
+            {
+                resposta[i].classList.add("certo");
+            }
+        }
+    }
+    function scrollar(){
+      let proximaResposta = document.querySelector(`.quadro div:nth-child(${contador2})`);
+      console.log(proximaResposta)
+      contador2++;
+      proximaResposta.scrollIntoView(true, {block: 'end',  behavior: 'smooth' });
+      console.log(contador2 + ' contagens')
+     
+    }
+    setTimeout(scrollar(), 2000)
+});
+
+responder();
+
+const botaoReiniciar = document.querySelector('.reiniciar')
+
+    botaoReiniciar.addEventListener("click", function(e)
+    {
+    const reiniciar = document.querySelector('div:last-child');
+
+    reiniciar.scrollIntoView({block: "end", behavior: "smooth"});
+
+    let resposta = document.querySelectorAll('.resposta')
+
+    for(i = 0; i < resposta.length; i++){
+
+        if(resposta[i].className.includes('certo')){
+            resposta[i].classList.remove('certo')
+    }
+        else if(resposta[i].className.includes('errado')){
+            resposta[i].classList.remove('errado')
+    }
+
+    if(resposta[i].className.includes('desabilitado')){
+            resposta[i].classList.remove('desabilitado')
+    }
+        else if(resposta[i].className.includes('escolhido')){
+            resposta[i].classList.remove('escolhido')
+    }}
+    final = document.querySelector('.final')
+    remover = final.classList.add("escondido")
+    contador2 = 1;
+    responder()
+    })
+
+
+    const botaoHome = document.querySelector('.home')
+    
+    botaoHome.addEventListener("click", function(e){
+        window.location = '../tela1/index.html'})
+// PAGE 2 END
